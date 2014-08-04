55470a43d33d7a40d5e6329dd5faefe234eb5b1a
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-08-04 21:59:50 +0700

    Initial commit

---
+++ lib/Perinci/CmdLine/NonOO.pm
@@ -0,0 +1,57 @@
+package Perinci::CmdLine::NonOO;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Perinci::Sub::Gen::FromClass qw(gen_func_from_class);
+
+require Exporter;
+our @ISA       = qw(Exporter);
+our @EXPORT_OK = qw(run_cmdline_app);
+
+my $res = gen_func_from_class(
+    class  => 'Perinci::CmdLine::Lite',
+    method => 'run',
+    name   => 'run_cmdline_app',
+);
+die "Can't create run_cmdline_app(): $res->[0] - $res->[1]"
+    unless $res->[0] == 200;
+
+1;
+# ABSTRACT: Non-OO interface for Perinci::CmdLine
+
+=for Pod::Coverage .+
+
+=head1 SYNOPSIS
+
+ use Perinci::CmdLine::NonOO qw(run_cmdline_app);
+ run_cmdline_app(url => '/Foo/bar');
+
+which is equivalent to:
+
+ use Perinci::CmdLine::Lite;
+ my $cli = Perinci::CmdLine::Lite->new(url => '/Foo/bar');
+ $cli->run;
+
+
+=head1 STATUS
+
+Experimental, proof of concept.
+
+
+=head1 DESCRIPTION
+
+L<Perinci::CmdLine> (or its alternatives L<Perinci::CmdLine::Lite>,
+L<Perinci::CmdLine::Any>) is a command-line application framework. It "exports"
+your functions as a CLI application. However, Perinci::CmdLine itself has an OO
+interface, which can be seen as ironic. This module is an attempt to fix this.
+
+
+=head1 SEE ALSO
+
+L<Perinci::CmdLine>, L<Perinci::CmdLine::Lite>, L<Perinci::CmdLine::Any>
+
+=cut
